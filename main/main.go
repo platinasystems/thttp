432eae1b4d3aac2093b1367f8d7412f166015726
Eliot Dresselhaus <edressel@platinasystems.com>
2015-10-21 11:14:45 -0700

    add put & append methods
    Signed-off-by: Eliot Dresselhaus <edressel@platinasystems.com>

--- main/main.go
+++ main/main.go
@@ -17,10 +17,11 @@ func main() {
 	fs.BoolVar(&w.Stdout, "stdout", true, "Fetch file and send to standard output")
 	fs.StringVar(&w.ServePath, "serve", "", "Directory to serve files from as file server")
 	fs.StringVar(&w.ServePort, "port", "9090", "TCP port for file server")
+	fs.StringVar(&w.PutPath, "put", "", "URL to http PUT standard input")
 	fs.Parse(os.Args[1:])
 
 	w.GetPaths = fs.Args()
-	if len(w.GetPaths) == 0 && w.ServePath == "" {
+	if len(w.GetPaths) == 0 && len(w.PutPath) == 0 && w.ServePath == "" {
 		fmt.Printf("Usage: wget OPTIONS FILES...\n")
 		fs.PrintDefaults()
 		os.Exit(1)
--- wget.go
+++ wget.go
@@ -6,13 +6,16 @@
 package wget
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"path"
+	"path/filepath"
 	"sync"
 )
 
@@ -20,6 +23,7 @@ type Config struct {
 	ServePath string
 	ServePort string
 	GetPaths  []string
+	PutPath   string
 	Verbose   bool
 	// Output to stdout instead of file
 	Stdout bool
@@ -55,15 +59,46 @@ func (w *Config) Get(u *url.URL) (err error) {
 	return
 }
 
+func (w *Config) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		http.FileServer(http.Dir(w.ServePath))
+	case "PUT", "APPEND":
+		var (
+			f   *os.File
+			err error
+		)
+		path := filepath.Join(w.ServePath, req.URL.Path)
+		if req.Method == "APPEND" {
+			f, err = os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+		} else {
+			f, err = os.Create(path)
+		}
+		if err != nil {
+			http.Error(res, fmt.Sprintf("create fails %s", err), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+		_, err = io.Copy(f, req.Body)
+		if err != nil {
+			http.Error(res, fmt.Sprintf("copy fails %s", err), http.StatusInternalServerError)
+			return
+		}
+		res.WriteHeader(http.StatusOK)
+	default:
+		http.Error(res, fmt.Sprintf("unknown method: %s", req.Method), http.StatusBadRequest)
+	}
+}
+
 func (w *Config) Run() (err error) {
 	// Must either have files to fetch or a directory to serve.
-	if len(w.GetPaths) == 0 && w.ServePath == "" {
+	if len(w.GetPaths) == 0 && len(w.PutPath) == 0 && w.ServePath == "" {
 		return fmt.Errorf("no files to fetch and no directory to serve")
 	}
 
 	var wg sync.WaitGroup
 	if w.ServePath != "" {
-		http.Handle("/", http.FileServer(http.Dir(w.ServePath)))
+		http.Handle("/", w)
 		var listener net.Listener
 		listener, err = net.Listen("tcp", ":"+w.ServePort)
 		if err != nil {
@@ -97,5 +132,32 @@ func (w *Config) Run() (err error) {
 			return
 		}
 	}
+
+	if len(w.PutPath) > 0 {
+		c := &http.Client{}
+		var (
+			req      *http.Request
+			res      *http.Response
+			contents []byte
+		)
+		contents, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return
+		}
+		url := w.PutPath
+		req, err = http.NewRequest("PUT", url, bytes.NewReader(contents))
+		req.ContentLength = int64(len(contents))
+		res, err = c.Do(req)
+		if err != nil {
+			return
+		}
+		defer res.Body.Close()
+		if res.StatusCode != http.StatusOK {
+			msg, _ := ioutil.ReadAll(res.Body)
+			err = fmt.Errorf("put %s: %s", url, msg)
+			return
+		}
+	}
+
 	return
 }
